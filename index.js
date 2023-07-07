4cf4376d5914176075f5d1a8210872b7acb1057b
VladSimonyan <129136870+VladSimonyan@users.noreply.github.com>
2023-07-06 18:47:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
-const router = require('express').Router();
-const apiRoutes = require('./api');
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
 
-router.use('/api', apiRoutes);
 
-router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
 });
 
-module.exports = router;
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
+
+
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    foreignKey: 'tag_id'
+  },
+  as: 'product_tags'
+});
+
+
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    foreignKey: 'product_id'
+  },
+  as: 'products'
+})
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
